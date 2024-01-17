140490d74968fc9b0a8d21c863d77dbf028e5ca5
RedInTheHead <122378710+RedInTheHead@users.noreply.github.com>
2024-01-17 10:53:55 -0500

    initial commit

---
+++ src/test/java/edu/bsu/cs/LeapYearCalculatorTest.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LeapYearCalculatorTest {
+    @Test
+    public void test2024isLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(2024);
+        Assertions.assertTrue(result);
+
+    }
+}
